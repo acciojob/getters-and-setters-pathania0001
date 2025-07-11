6684c0b17f50e90d006e7e4b62c1241400af99e5
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-11 13:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+     this._name = name;
+	 this._age = age;
+	}
+	
+	get name(){
+      return this._name;
+	}
 
-class Student extends Person {}
+    set name(name){
+		this._name=name;
+	}
+	
+	get age(){
+      return this._age;
+	}
+	set age(age){
+		this._age=age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	constructor(name , age){
+		super(name,age);
+	}
+	study(){
+		console.log(`${this._name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	constructor(name,age){
+      super(name,age);
+	}
+	teach(){
+      console.log(`${this._name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
